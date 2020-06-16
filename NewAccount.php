e24148d42472ce8d3a58433848f019927cb377c0
unknown <theekshana.18@cse.mrt.ac.lk>
2020-06-16 22:10:51 +0530

    quatation marks

--- NewAccount.php
+++ NewAccount.php
@@ -11,13 +11,13 @@
     
 <form action = "NewAccount.php" method="POST">    
     <pre id="container-fluid text-left"><h4><center>
- NATIONAL ID NO : <input name="ID" type="text" ><br>
- FIRST_NAME     : <input name="first_name" type="text" ><br>
- SECOND_NAME    : <input name="second_name" type="text" ><br>
- ADDRESS        : <input name="address" type="text" ><br>
- TELEPHONE      : <input name="telephone" type="text" ><br>
- EMAIL ADDRESS   : <input name="email" type="text" > <br>
- SCHOOL         : <input name="school" type="text" >
+ NATIONAL ID NO : <input name="ID" type="text" id=""><br>
+ FIRST_NAME     : <input name="first_name" type="text" id=""><br>
+ SECOND_NAME    : <input name="second_name" type="text" id=""><br>
+ ADDRESS        : <input name="address" type="text" id=""><br>
+ TELEPHONE      : <input name="telephone" type="text" id=""><br>
+ EMAIL ADDRESS   : <input name="email" type="text" id=""> <br>
+ SCHOOL         : <input name="school" type="text" id="">
    
  OCCUPATION    :          <select name="occupation">
  <option value="teacher">teacher</option>
@@ -26,55 +26,68 @@
 
             </select>
     <br>
- enter   password : <input name="password1" type="password" ><br>
- confirm password : <input name="password2" type="password" ><br>   
+ enter   password : <input name="password1" type="password" id=""><br>
+ confirm password : <input name="password2" type="password" id=""><br>   
 <input name="submit" type="submit" id=""></h4></center></pre>
 </form>
 <div class="alert">   
 <?php
 
+    
 
+  
+    if (isset($_POST['submit'])) {
+
+    $query_id= "SELECT ID FROM userdetails WHERE ID='{$_POST['ID']}'";  
+    
     $query_user="INSERT INTO userdetails 
-        VALUES('{$_POST[ID]}','{$_POST[first_name]}','{$_POST[second_name]}','{$_POST[address]}',
-               '{$_POST[telephone]}','{$_POST[email]}','{$_POST[occupation]}','{$_POST[school]}')";
+    VALUES('{$_POST['ID']}','{$_POST['first_name']}','{$_POST['second_name']}','{$_POST['address']}',
+           '{$_POST['telephone']}','{$_POST['email']}','{$_POST['occupation']}','{$_POST['school']}','{$_POST['occupation']}')";
 
     $query_password="INSERT INTO passwords 
-        VALUES('{$_POST[ID]}','{$_POST[password1]}')";
+        VALUES('{$_POST["ID"]}','{$_POST["password1"]}')";
 
-    $query_id= "SELECT ID FROM user WHERE ID='{$_POST[ID]}'";
-
-    if (isset($_POST['submit'])) {
-        
-        if($_POST[ID]!=null){
+        if($_POST['ID']!=null){
                 
-            if ($_POST[password1]==$_POST[password2] and $_POST[password1]!=null ) {
+            if ($_POST['password1']==$_POST['password2'] and $_POST['password1']!=null ) {
                 //echo"re entered password is correct<br>";
-
+                
+                        
                 $id_result= mysqli_query($connection,$query_id);
+                
                 
                 if ($id_result){
                     $id_details=mysqli_num_rows($id_result);
                     //echo $id_details; 
 
                     if ($id_details==null){
+
+                       
+
                         $result= mysqli_query($connection,$query_user);
                         $pass  = mysqli_query($connection,$query_password);   
             
-                        if ($result and $pass) {
+                        //16
+                        echo $_POST['ID']  ;   
+                                            
+                        echo $_POST['password1'] ;
+                                 
+                        if ($pass) {
+                            if ($result) {
                             echo"account created successfully<br><hr>";
-                            echo'<a href="home.php">Home Page</a>';                                        
+                            echo'<a href="home.php">Home Page</a>';       }                                 
                         }
 
-                        else  echo"account creation failed<br><hr>";  
+                        else  echo"account creation failed no pass<br><hr>";  
                     
                     }
                     else echo "Already account exists!";
                 
                 }    
-                else  echo"account creation failed<br><hr>";  
+                else  echo"account creation failed 2<br><hr>";  
 
             }
-            elseif ($_POST[password1]==null){
+            elseif ($_POST['password1']==null){
                     echo"password can not be empty.<hr>";    
             }
 
