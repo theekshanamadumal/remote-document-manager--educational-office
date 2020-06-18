f4ead67f1715de7800139817d80e82cbae121cd8
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2020-06-18 09:28:51 +0530

    Add files via upload

--- AdminEdit.php
+++ AdminEdit.php
@@ -20,24 +20,53 @@ $query= "SELECT * FROM requestslist";
 $result= mysqli_query($connection,$query);
 $count = mysqli_num_rows($result);
 
-if ($count==TRUE){
+if ($result-> num_rows > 0){?>
+  <div class="limiter">
+  <div class="container-table100">
+  <div class="wrap-table100">
+                <div class="table100">
+                  <table>
+                    <thead>
+                        <tr class="table100-head">
+                        <th class="column1">Request NO        :</th>
+                        <th class="column2">Request ID         :</th>
+                        <th class="column3">ID            :</th>
+                        <th class="column4" >Status          :</th>
+                        <th class="column5" >Description          :</th>
+                        </tr>
+                    </thead>
+                    <tbody>
     
-    while($row=$result->fetch_assoc()){
-         echo $row["requestNo"]."   ".$row["requestId"]."   ".$row["ID"]."   ".$row["status"].$row["description"]."<br>";
+    <?php
+    while($row = $result-> fetch_assoc()){
+        echo "<tr><td class='column1' >" .$row["requestNo"]."</td><td class='column2' >" .$row["requestId"]."</td><td class='column3'>".$row["ID"] ."</td><td class='column4'>".$row["status"] . "</td><td class='column5'>".$row["description"]."</td></tr>";
+        
+      }
+    echo "</tbody>";
+    echo "</table>";
+    echo"</div>";
+    echo"</div>";
+    echo"</div>";
+    echo"</div>";
+    //echo"<div class=' col-sm'></div>";
+     
     
     
     
-    }
-}
+
+  }
+
+  //Generate Message if no jobs are submitted yet
+
 else{
-    echo "No Data in the Database";
+    echo "No Requests are Available";
 }
 
 ?>
 <br/>
 <br/>
 <br/>
-<button type="button" class="btn btn-default"><a href="approve.php" target="_self">APPROVE ALL REQUESTS</a></button>
+<button type="button" class="btn btn-default"><a href="approve.php" target="_self">APPROVE A REQUEST</a></button>
   <?php    include_once('inc/foot.php');    ?>
   </body>
   </html>
