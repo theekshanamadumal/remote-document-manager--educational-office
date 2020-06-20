3880eee174307ab938d47ab54befb84fcc64fe3c
Ayodya-erandi <66939752+Ayodya-erandi@users.noreply.github.com>
2020-06-20 11:59:36 +0530

    Add files via upload

--- inc/foot.php
+++ inc/foot.php
@@ -1,10 +1,10 @@
-
 <footer class="page-footer font-medium blue" style="padding-top:10px;">
 
-
-  <div class="footer-copyright text-right py-3" style=" font-size: large;"><b>Admin Login:</b>
+	<?php if(!isset($_SESSION['loginID'])){?>
+    <div class="footer-copyright text-right py-3" style=" font-size: large;"><b>Admin Login:</b>
     	<a style="color:gray; font-size: large;" href="adminLogin.php">adminLogin</a>
-  </div>
+  	</div>
+  	<?php } ?>
   <br>
   <div class="HotLine">
     <p><b>Hot Line</b> : 037-222-0345<br>
